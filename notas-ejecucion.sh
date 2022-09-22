aa428750e506e38aad8cf69c83537684e4383907
Isaac Mendoza <imendozak24@gmail.com>
2022-09-22 00:03:14 -0500

    Create notas-ejecucion.sh

---
+++ notas-ejecucion.sh
@@ -0,0 +1,13 @@
+#ya pude desplegar sin esos errores
+#solo tuve q probar con esta version de pnpm
+node --version                            
+v14.18.1
+pnpm --version                            
+6.7.1
+
+#usas NVM para tener varios nodejs y eliges v14.18.1
+#instalas Node.js v14.18.1
+nvm install 14.18.1
+nvm use 14.18.1
+#instalar la version exacta para este proyecto
+npm install -g pnpm@6.7.1
